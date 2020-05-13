5da36acbc7b05a0adf1240a1526a97e0e3308bc2
anthonyca18m <anthonyca18m@gmail.com>
2020-05-13 14:20:04 -0500

    All we need is JWT for Laravel API Authentication

--- proyect_one/app/Http/Controllers/API/UserController.php
+++ proyect_one/app/Http/Controllers/API/UserController.php
@@ -11,6 +11,15 @@ use Carbon\Carbon;
 class UserController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
