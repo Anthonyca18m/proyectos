cffaa950e58b16f8c94085cba2be2b5e4b5c49f8
anthonyca18m <anthonyca18m@gmail.com>
2020-05-16 20:08:51 -0500

    Configuración de vfrom

--- proyecto_tickects/resources/js/app.js
+++ proyecto_tickects/resources/js/app.js
@@ -1,7 +1,18 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { 
+    HasError,
+    AlertError,
+    AlertErrors, 
+    AlertSuccess
+  } from 'vform'
 
+window.Form = Form
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+Vue.component(AlertErrors.name, AlertErrors)
+Vue.component(AlertSuccess.name, AlertSuccess)
 
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
